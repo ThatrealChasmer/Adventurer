520ed7de08502f2f0ada66c1c966a72d55fc9de5
Chasmer <36886220+ThatrealChasmer@users.noreply.github.com>
2019-07-30 00:46:43 +0200

    Functional Menu

--- Adventurer/Assets/Scripts/MainCombatMenu.cs
+++ Adventurer/Assets/Scripts/MainCombatMenu.cs
@@ -6,10 +6,12 @@ public class MainCombatMenu : MonoBehaviour
 {
     static MainCombatMenu instance;
     public GameObject indicator;
-    public List<Category> LeftCategories;
-    public List<Category> RightCategories;
+    public Vector3 indpos;
+    public List<List<Category>> lists = new List<List<Category>>();
+    public List<Category> LeftCategories = new List<Category>();
+    public List<Category> RightCategories = new List<Category>();
     public Category pickedOption;
-    public string currentList;
+    public int currentList;
     public int currentIndex;
 
     public enum Category{
@@ -22,26 +24,48 @@ public class MainCombatMenu : MonoBehaviour
     void Start()
     {
         instance = this;
+        indicator = this.gameObject;
+        indpos = indicator.transform.position;
         LeftCategories.Add(Category.Attack);
         LeftCategories.Add(Category.Items);
         RightCategories.Add(Category.Spells);
         RightCategories.Add(Category.Run);
-        currentList = "LeftCategories";
+        lists.Add(LeftCategories);
+        lists.Add(RightCategories);
+        currentList = 0;
         currentIndex = 0;
-        pickedOption = LeftCategories[currentIndex];
-        pickedOption = 
+        pickedOption = lists[currentList][currentIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentList == "LeftCategories")
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if(Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(KeyCode.RightArrow) && currentList < lists.Count - 1)
+            {
+                currentList++;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && currentList > 0)
+            {
+                currentList--;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && currentIndex < lists[currentList].Count - 1)
             {
                 currentIndex++;
-                pickedOption = LeftCategories[currentIndex];
             }
+            else if (Input.GetKeyDown(KeyCode.UpArrow) && currentIndex > 0)
+            {
+                currentIndex--;
+            }
+            pickedOption = lists[currentList][currentIndex];
+            MoveIndicator(currentList, currentIndex);
         }
+    }
+
+    public void MoveIndicator(float x, float y)
+    {
+        indicator.transform.position = indpos + new Vector3(x*200, + y*-55, 0);
+        Debug.Log(indicator.transform.position.x + ", " + indicator.transform.position.y);
     }
 }

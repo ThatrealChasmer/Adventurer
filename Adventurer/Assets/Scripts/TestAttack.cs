24a813eaa9a408bf88e2612ae7d01b7b9730e0df
Aleksander Mielczarek <40267367+15465@users.noreply.github.com>
2019-07-31 00:34:08 +0200

    update

--- Adventurer/Assets/Scripts/TestAttack.cs
+++ Adventurer/Assets/Scripts/TestAttack.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class TestAttack : MonoBehaviour
 {
-    GameObject enemy;
     public void SkillUse()
     {
         //find enemy
-        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
         //call Damage(10) from EnemyHealthSystem
         enemy.GetComponent<EnemyHealthSystem>().Damage(10);
         Debug.Log("skill used");
-    }
-    void Start()
-    {
-        SkillUse();
     }
 }

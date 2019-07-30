3f1645762cda483d8091f23a7538bb077b1e4345
Aleksander Mielczarek <40267367+15465@users.noreply.github.com>
2019-07-31 00:37:52 +0200

    scena

---
+++ Adventurer/Assets/Scripts/TestAttack1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAttack1 : MonoBehaviour
+{
+    public void SkillUse()
+    {
+        //find enemy
+        GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+        //call Damage(10) from EnemyHealthSystem
+        enemy.GetComponent<EnemyHealthSystem>().Damage(10);
+        Debug.Log("skill used");
+    }
+}
--- Adventurer/Assets/Scripts/TestAttack2.cs
+++ Adventurer/Assets/Scripts/TestAttack2.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestAttack : MonoBehaviour
+public class TestAttack2 : MonoBehaviour
 {
     public void SkillUse()
     {
         //find enemy
         GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
         //call Damage(10) from EnemyHealthSystem
-        enemy.GetComponent<EnemyHealthSystem>().Damage(10);
+        enemy.GetComponent<EnemyHealthSystem>().Damage(15);
         Debug.Log("skill used");
     }
 }

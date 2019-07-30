0fc3c2706cf3349eef920f19d7130d55d43bdf91
Aleksander Mielczarek <40267367+15465@users.noreply.github.com>
2019-07-31 00:32:34 +0200

    testattack

---
+++ Adventurer/Assets/Scripts/TestAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAttack : MonoBehaviour
+{
+    GameObject enemy;
+    public void SkillUse()
+    {
+        //find enemy
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        //call Damage(10) from EnemyHealthSystem
+        enemy.GetComponent<EnemyHealthSystem>().Damage(10);
+        Debug.Log("skill used");
+    }
+    void Start()
+    {
+        SkillUse();
+    }
+}

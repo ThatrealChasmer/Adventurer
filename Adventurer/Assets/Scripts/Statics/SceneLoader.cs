eb82be7137950b343c74202c990850ff48a4de3a
Chasmer <36886220+ThatrealChasmer@users.noreply.github.com>
2019-08-03 01:52:27 +0200

    Added SceneLoader

---
+++ Adventurer/Assets/Scripts/Statics/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public static void Load(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}

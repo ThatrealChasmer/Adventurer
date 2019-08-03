65968e94991bd1d3d32a4e9da7747fc497d23ab2
Aleksander Mielczarek <40267367+15465@users.noreply.github.com>
2019-08-04 01:38:45 +0200

    upgrade

--- Adventurer/Assets/Scripts/EnemyTestAI.cs
+++ Adventurer/Assets/Scripts/EnemyTestAI.cs
@@ -14,11 +14,13 @@ public class EnemyTestAI : MonoBehaviour
 
     private void Action(int index)
     {
-        //if (index == MyIndex){}
-        Debug.Log("Enemy Action");
-        PlayerStats.TakeDamage(10);
-        Debug.Log(PlayerStats.playerStats.currentHealth);
-        TurnSystem.EndTurn();
+        if (index == GetComponent<EnemyStats>().TurnIndex)
+        {
+            Debug.Log("Enemy Action");
+            PlayerStats.TakeDamage(10);
+            Debug.Log(PlayerStats.playerStats.currentHealth);
+            TurnSystem.EndTurn();
+        }
     }
 
 

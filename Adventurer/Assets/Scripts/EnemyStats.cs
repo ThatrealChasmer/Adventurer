0bef5e77ffd1000a6ffc8e7be198ac0c57b1ac61
Chasmer <36886220+ThatrealChasmer@users.noreply.github.com>
2019-08-04 01:36:54 +0200

    Upgraded BattleManager

--- Adventurer/Assets/Scripts/EnemyStats.cs
+++ Adventurer/Assets/Scripts/EnemyStats.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class EnemyStats : MonoBehaviour
 {
-    int TurnIndex;
-    int Strength;
+    public int TurnIndex;
+
+    public int strength;
+    public int defense;
+    public int perception;
+    public int inteligence;
+    public int speed;
+    public int luck;
+
+    public StatsTemplate.Stats stats;
+
+    private void Start()
+    {
+        stats = new StatsTemplate.Stats(strength, defense, perception, inteligence, speed, luck);
+    }
+
 }
